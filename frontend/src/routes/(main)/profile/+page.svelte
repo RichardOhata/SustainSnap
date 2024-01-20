49132a7d84aa526d6f9fb874356735292ad06641
jaytoven <jmuhigi@student.ubc.ca>
2024-01-20 14:04:24 -0800

    added profile and user

--- frontend/src/routes/(main)/profile/+page.svelte
+++ frontend/src/routes/(main)/profile/+page.svelte
@@ -1,7 +1,21 @@
 <script>
-   let name = 'Profile';
+    export let user = {
+        name: "John Doe",
+        username: "johndoe",
+        weekly rank: 0,
+        monthly rank: 0,
+        all time rank: 0,
+        snaps taken: 0,
+        total points: 0,
+        start date: 0000-00-00,
+    };
 </script>
 
-<main>
-    <h1>Hello World</h1>
-</main>
+<style>
+    .profile {
+        max-width: 430;
+        max-height: 932;
+        margin: 0 auto;
+        padding: 20px;
+    }
+</style>
