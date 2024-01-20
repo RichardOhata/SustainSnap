97da29f53347c83b67105f8b1bbe2b0c358c840e
RichardOhata <richardohata@gmail.com>
2024-01-20 12:05:27 -0800

    Created express boilerplate

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+// Import required modules
+const express = require('express');
+
+// Create an Express application
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to parse incoming JSON requests
+app.use(bodyParser.json());
+
+// Sample route
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+
+// Sample API route
+app.post('/api/data', (req, res) => {
+  const { data } = req.body;
+  // Process the data and send a response
+  res.json({ message: `Received data: ${data}` });
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
