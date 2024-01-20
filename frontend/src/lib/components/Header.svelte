4c9cd6c11c97ab3d4802ea1fc8fad9c10085b78a
devnull03 <56480041+devnull03@users.noreply.github.com>
2024-01-20 13:37:54 -0800

    feat: attributes for icons

---
+++ frontend/src/lib/components/Header.svelte
@@ -0,0 +1,17 @@
+<script>
+  import User from "$lib/icons/User.svelte";
+  import { goto } from "$app/navigation";
+</script>
+
+<header class="border-b shadow-md fixed top-0 w-full bg-green-600 p-4">
+  <div class="flex flex-row w-full justify-between items-center">
+    <img src="" alt="logo" />
+    <span class="text-xl text-white font-semibold"> Lorem ipsum </span>
+    <button
+      class="transition duration-150 ease-in-out"
+      on:click={() => goto("/profile")}
+    >
+      <User />
+    </button>
+  </div>
+</header>
--- frontend/src/lib/components/Nav.svelte
+++ frontend/src/lib/components/Nav.svelte
@@ -2,17 +2,11 @@
   import Camera from "$lib/icons/Camera.svelte";
   import Gallery from "$lib/icons/Gallery.svelte";
   import Glossary from "$lib/icons/Glossary.svelte";
-  import User from "$lib/icons/User.svelte";
   import Leaderboard from "$lib/icons/Leaderboard.svelte";
   import { page } from "$app/stores";
   import { goto } from "$app/navigation";
 
   let navItems = [
-    {
-      path: "/profile",
-      name: "User",
-      icon: User,
-    },
     {
       path: "/leaderboard",
       name: "Leaderboard",
@@ -28,15 +22,10 @@
       name: "Gallery",
       icon: Gallery,
     },
-    {
-      path: "/glossary",
-      name: "Glossary",
-      icon: Glossary,
-    },
   ];
 </script>
 
-<nav class="border-t shadow fixed bottom-0 w-full bg-green-600 pb-8 pt-4">
+<nav class="border-t shadow-md fixed bottom-0 w-full bg-green-600 pb-8 pt-4">
   <div class="flex flex-row w-full justify-evenly items-center">
     {#each navItems as navItem}
       <button
