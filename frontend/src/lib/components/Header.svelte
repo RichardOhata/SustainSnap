58ec5849a763b89e0dc165836613df13f06a41a0
jaytoven <jmuhigi@student.ubc.ca>
2024-01-21 11:41:39 -0800

    changed clean sweep to sustain snap

--- frontend/src/lib/components/Header.svelte
+++ frontend/src/lib/components/Header.svelte
@@ -7,7 +7,7 @@
 <header class="border-b shadow-md fixed top-0 w-full bg-[#223D23] p-4 z-50">
   <div class="flex flex-row w-full justify-between items-center">
     <Plant />
-    <span class="text-xl text-white font-lemon"> Clean Sweep </span>
+    <span class="text-xl text-white font-lemon"> Sustain Snap </span>
     <button
       class="transition duration-150 ease-in-out border border-white rounded-full object-cover"
       on:click={() => {
