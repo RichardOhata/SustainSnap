c00f3ba5ccbba583dc965e00a1d8baf6a3b818d9
jaytoven <jmuhigi@student.ubc.ca>
2024-01-20 22:10:32 -0800

    - added leaderboard & profile

---
+++ frontend/src/routes/(main)/leaderboard/+page.svelte
@@ -0,0 +1,67 @@
+<script lang='ts'>
+    let user = { 
+      name: "John Doe",
+      weeklyRank: 0,
+      monthlyRank: 0,
+      allTimeRank: 0,
+      snapsTaken: 0,
+      totalPoints: 0,
+      startDate: "2024-01-20",
+      imageUrl: "/trophy.png"
+  }
+</script>
+
+<style>
+  .leaderboard {
+      max-width: 100%;
+      text-align: center;
+      margin: 0 auto;
+      padding: 3px;
+      background-color: #1b3a0d;
+      text-align: center;
+  }
+
+  .trophy {
+      width: 15%;
+      margin: 0 auto;
+      padding: 5px;
+  }
+
+  .leaderboard-entry {
+      display: flex;
+      font-size: 0.9rem;
+      color: white;
+      margin-bottom: 10px;
+      max-width: 500px;
+      margin: auto;
+      padding: 25px;
+      align-items: center;
+      justify-content: space-around;
+      border: 1 px solid white;
+      border-radius: 8px;
+      background-color: rgba(40,60,36);
+  }
+
+  .leaderboard-header {
+      font-size: 1.5rem;
+      color: white;
+      font-weight: bold;
+  }
+
+</style>
+
+<div class="leaderboard">
+  <h1 class="leaderboard-header">Leaderboard</h1>
+  <img class="trophy" src={user.imageUrl} alt="trophy">
+  <p class="leaderboard-entry">1. Jane Doe &nbsp; &nbsp; &nbsp; 1000000</p>
+  <br>
+  <p class="leaderboard-entry">2. Jane Doe &nbsp; &nbsp; &nbsp; 750000</p>
+  <br>
+  <p class="leaderboard-entry">3. Jane Doe &nbsp; &nbsp; &nbsp; &nbsp; 650000</p>
+  <br>
+  <p class="leaderboard-entry">4. Jane Doe &nbsp; &nbsp; &nbsp; 500000</p>
+  <br>
+  <p class="leaderboard-entry">5. Jane Doe &nbsp; &nbsp; &nbsp; 350000</p>
+  <br>
+  <p class="leaderboard-entry">6. Jane Doe &nbsp; &nbsp; &nbsp; 150000</p>
+</div>
--- frontend/src/routes/(main)/profile/+page.svelte
+++ frontend/src/routes/(main)/profile/+page.svelte
@@ -1,65 +1,83 @@
 <script lang='ts'>
-      let user = { 
-        name: "John Doe",
-        weeklyRank: 0,
-        monthlyRank: 0,
-        allTimeRank: 0,
-        snapsTaken: 0,
-        totalPoints: 0,
-        startDate: "2024-01-20",
-        imageUrl: "/blank.png"
-    }
+    let user = { 
+      name: "John Doe",
+      weeklyRank: 1,
+      monthlyRank: 22,
+      allTimeRank: 112,
+      snapsTaken: 100,
+      totalPoints: 450000,
+      startDate: "2024-01-20",
+      imageUrl: "/blank.png"
+  }
 </script>
 
 <style>
-    .profile {
-        max-width: 600px;
-        width: 100%;
-        text-align: center;
-        margin: 0 auto;
-        padding: 10px;
-        background-color: #fff;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        border-radius: 8px;
-        text-align: center;
-    }
+  .profile {
+      max-width: 100%;
+      text-align: center;
+      margin: 0 auto;
+      padding: 20px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+      border-radius: 8px;
+      text-align: center;
+  }
 
-    .profile-image {
-        width: 15%;
-        margin: 0 auto;
-    }
-    
-    .user-name {
-        font-size: 1rem;
-        margin-bottom: 10px;
-    }
-    .user-info {
-        display: flex;
-        font-size: 0.9rem;
-        margin-bottom: 10px;
-        max-width: 350px;
-        margin: auto;
-        padding: 20px;
-        align-items: center;
-        justify-content: space-around;
-        border: 1 px solid white;
-        border-radius: 8px;
-        background-color: forestgreen;
-    }
+  .profile-image {
+      width: 15%;
+      margin: 0 auto;
+  }
+  
+  .user-name {
+      font-size: 1rem;
+      margin-bottom: 10px;
+  }
+  .user-info {
+      display: flex;
+      font-size: 0.9rem;
+      color: grey;
+      margin-bottom: 10px;
+      max-width: 350px;
+      margin: auto;
+      padding: 20px;
+      align-items: center;
+      justify-content: space-around;
+      border: 1 px solid white;
+      border-radius: 8px;
+  }
 
-    .bio {
-        margin-top: 20px;
-    }
+  .divider {
+  border-top: 1px solid #ccc;
+  margin-top: 5px; /* Adjust margin as needed */
+  margin-bottom: 5px; /* Adjust margin as needed */
+}
+
+  .bio {
+      margin-top: 20px;
+  }
+
+  .me {
+      font-size: 1.4rem;
+      color: grey;
+      font-weight: bold;
+  }
+
+  .name {
+      font-size: 0.8rem;
+      color: grey;
+      font-weight: 600;
+  }
 </style>
 
 <div class="profile">
-    <h1>Me</h1>
-    <img class="profile-image" src={user.imageUrl} alt="Profile">
-    <p class="user-name">{user.name}</p>
-    <p class="user-info">Weekly Rank: {user.weeklyRank}</p>
-    <p class="user-info">Monthly Rank: {user.monthlyRank}</p>
-    <p class="user-info"> All Time Rank: {user.allTimeRank}</p>
-    <p class="user-info"> Snaps Taken: {user.snapsTaken}</p>
-    <p class="user-info"> Total Points: {user.totalPoints}</p>
-    <p class="user-info"> Start Date: {user.startDate}</p>
+  <h1 class="me">Me</h1>
+  <img class="profile-image" src={user.imageUrl} alt="Profile">
+  <p class="name">{user.name}</p>
+  <p class="user-info">
+      Weekly Rank {user.weeklyRank}</p>
+  <p class="user-info">Monthly Rank {user.monthlyRank}</p>
+  <p class="user-info"> All Time Rank {user.allTimeRank}</p>
+  <div class="divider"></div>
+  <p class="user-info"> Snaps Taken {user.snapsTaken}</p>
+  <p class="user-info"> Total Points {user.totalPoints}</p>
+  <p class="user-info"> Start Date {user.startDate}</p>
 </div>
